cac10fcef87ce3d505b3312c2686fc4067d99c62
pbing <bernd.beuster@gmail.com>
2016-02-25 22:36:03 +0100

    Update hasti_xbar.sv

--- rtl/hasti_xbar.sv
+++ rtl/hasti_xbar.sv
@@ -6,36 +6,36 @@ module hasti_xbar
    if_hasti_master_io.f m0, m1,
    if_hasti_slave_io.f  s0, s1);
 
-   if_hasti_slave_io sl00();
-   if_hasti_slave_io sl01();
-   if_hasti_slave_io sl10();
-   if_hasti_slave_io sl11();
+   if_hasti_slave_io m0s0();
+   if_hasti_slave_io m0s1();
+   if_hasti_slave_io m1s0();
+   if_hasti_slave_io m1s1();
 
    hasti_bus hasti_bus_0
      (.hclk,
       .hresetn,
       .m(m0),
-      .s0(sl00),
-      .s1(sl01));
+      .s0(m0s0),
+      .s1(m0s1));
 
    hasti_bus hasti_bus_1
      (.hclk,
       .hresetn,
       .m(m1),
-      .s0(sl10),
-      .s1(sl11));
+      .s0(m1s0),
+      .s1(m1s1));
 
    hasti_slave_mux hasti_slave_mux_0
      (.hclk,
       .hresetn,
-      .in1(sl00),
-      .in2(sl10),
+      .in1(m0s0),
+      .in2(m1s0),
       .out(s0));
 
    hasti_slave_mux hasti_slave_mux_1
      (.hclk,
       .hresetn,
-      .in1(sl01),
-      .in2(sl11),
+      .in1(m0s1),
+      .in2(m1s1),
       .out(s1));
 endmodule
