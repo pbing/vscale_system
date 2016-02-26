434baa94861d02692226fbd4f402fa8a6c9dff58
Bernd Beuster <bernd.beuster@gmail.com>
2016-02-26 21:55:47 +0100

    Start enumeration always with zero.

--- rtl/hasti_slave_mux.sv
+++ rtl/hasti_slave_mux.sv
@@ -3,28 +3,28 @@
 module hasti_slave_mux
    (input wire          hclk,
     input wire          hresetn,
-    if_hasti_slave_io.n in1, in2,
-    if_hasti_slave_io.f out);
+    if_hasti_slave_io.n in0, in1, // from differen masters
+    if_hasti_slave_io.f out);     // to slave
 
    import pk_hasti::*;
 
    // FIXME
-   assign out.haddr	= in1.haddr;
-   assign out.hwrite	= in1.hwrite;
-   assign out.hsize	= in1.hsize;
-   assign out.hburst	= in1.hburst;
-   assign out.hprot	= in1.hprot;
-   assign out.htrans	= in1.htrans;
-   assign out.hmastlock = in1.hmastlock;
-   assign out.hwdata	= in1.hwdata;
-   assign in1.hrdata	= out.hrdata;
-   assign out.hsel	= in1.hsel;
-   assign out.hready	= in1.hready;
-   assign in1.hreadyout = out.hreadyout;
-   assign in1.hresp	= out.hresp;
+   assign out.haddr	= in0.haddr;
+   assign out.hwrite	= in0.hwrite;
+   assign out.hsize	= in0.hsize;
+   assign out.hburst	= in0.hburst;
+   assign out.hprot	= in0.hprot;
+   assign out.htrans	= in0.htrans;
+   assign out.hmastlock = in0.hmastlock;
+   assign out.hwdata	= in0.hwdata;
+   assign in0.hrdata	= out.hrdata;
+   assign out.hsel	= in0.hsel;
+   assign out.hready	= in0.hready;
+   assign in0.hreadyout = out.hreadyout;
+   assign in0.hresp	= out.hresp;
 
-   assign in2.hrdata	= '0;
-   assign in2.hreadyout = 1'b1;
-   assign in2.hresp	= OKAY;
+   assign in1.hrdata	= '0;
+   assign in1.hreadyout = 1'b1;
+   assign in1.hresp	= OKAY;
 
 endmodule
