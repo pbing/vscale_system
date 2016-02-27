12a49d54c64462c2c9d6275ef2187ed07700385b
Bernd Beuster <bernd.beuster@gmail.com>
2016-02-27 18:06:24 +0100

    Use addresses from package.

--- rtl/poci_led_driver.sv
+++ rtl/poci_led_driver.sv
@@ -4,14 +4,16 @@ module poci_led_driver
   (input wire              pclk,
    input wire              presetn,
    if_poci.f               bus,
-
    output logic [3:0][6:0] hex,   // 7-segment display
    output logic [7:0]      ledg,  // LED green
    output logic [9:0]      ledr); // LED red
 
+   import pk_poci::*;
+
    logic read_en;
    logic write_en;
-   logic sel_hex, sel_ledg, sel_ledr;
+
+   enum integer unsigned {SEL_HEX, SEL_LEDG, SEL_LEDR, NOSEL} sel;
 
    assign bus.pready  = 1'b1;
    assign bus.pslverr = 1'b0;
@@ -19,27 +21,34 @@ module poci_led_driver
    assign read_en  = bus.psel & ~bus.pwrite;                // assert for whole read transfer
    assign write_en = bus.psel &  bus.pwrite & ~bus.penable; // assert for 1st cycle of write transfer
 
-   assign sel_hex  = (bus.paddr[11:0] >= 12'h000 && bus.paddr[11:0] <= 12'h010);
-   assign sel_ledg = (bus.paddr[11:0] >= 12'h010 && bus.paddr[11:0] <= 12'h020);
-   assign sel_ledr = (bus.paddr[11:0] >= 12'h020 && bus.paddr[11:0] <= 12'h030);
+   /* decoder */
+   always_comb
+     if (bus.paddr[11:0] == addr_hex[11:0])
+       sel = SEL_HEX;
+     else if (bus.paddr[11:0] == addr_ledg[11:0])
+       sel = SEL_LEDG;
+     else if (bus.paddr[11:0] == addr_ledr[11:0])
+       sel = SEL_LEDR;
+     else
+       sel = NOSEL;
 
    /* multiplexor */
    always_comb
      if(read_en)
-       unique case (1'b1)
-	 sel_hex:
+       case (sel)
+	 SEL_HEX:
 	   begin
-	      bus.prdata[ 7-:8] = {1'b0, hex[0]};
-	      bus.prdata[15-:8] = {1'b0, hex[1]};
-	      bus.prdata[23-:8] = {1'b0, hex[2]};
-	      bus.prdata[31-:8] = {1'b0, hex[3]};
+	      bus.prdata[ 7-:8] = {1'b0, ~hex[0]};
+	      bus.prdata[15-:8] = {1'b0, ~hex[1]};
+	      bus.prdata[23-:8] = {1'b0, ~hex[2]};
+	      bus.prdata[31-:8] = {1'b0, ~hex[3]};
 	   end
 
-	 sel_ledg: bus.prdata = {24'b0, ledg};
+	 SEL_LEDG: bus.prdata = {24'b0, ledg};
 
-	 sel_ledr: bus.prdata = {22'b0, ledg};
+	 SEL_LEDR: bus.prdata = {22'b0, ledr};
 
-	 default bus.prdata = 'x;
+	 default   bus.prdata = 'x;
        endcase
      else
        bus.prdata = 'x;
@@ -48,7 +57,7 @@ module poci_led_driver
    always_ff @(posedge pclk)
      if (!presetn)
        hex <= '1;
-     else if (write_en && sel_hex)
+     else if (write_en && sel == SEL_HEX)
        begin
 	  hex[0] <= ~bus.pwdata[ 6-:7];
 	  hex[1] <= ~bus.pwdata[14-:7];
@@ -59,12 +68,12 @@ module poci_led_driver
    always_ff @(posedge pclk)
      if (!presetn)
        ledg <= '0;
-     else if (write_en && sel_ledg)
-       ledg <= ~bus.pwdata[7:0];
+     else if (write_en && sel == SEL_LEDG)
+       ledg <= bus.pwdata[7:0];
 
    always_ff @(posedge pclk)
      if (!presetn)
        ledr <= '0;
-     else if (write_en && sel_ledr)
-       ledr <= ~bus.pwdata[9:0];
+     else if (write_en && sel == SEL_LEDR)
+       ledr <= bus.pwdata[9:0];
 endmodule
