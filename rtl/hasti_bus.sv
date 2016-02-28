e70c22843747c0de6322e34e6e72d127504d923c
Bernd Beuster <bernd.beuster@gmail.com>
2016-02-28 10:01:59 +0100

    Added slave port s2 for I/O bridge.

--- rtl/hasti_bus.sv
+++ rtl/hasti_bus.sv
@@ -4,21 +4,23 @@ module hasti_bus
   (input wire           hclk,
    input wire           hresetn,
    if_hasti_master_io.f m,
-   if_hasti_slave_io.f  s0, s1);
+   if_hasti_slave_io.f  s0, s1, s2);
 
    import pk_hasti::*;
 
    logic       hready;
-   logic [1:0] hsel_r;
+   logic [2:0] hsel_r;
 
    /* decoder */
    assign s0.hsel = (m.haddr >= addr_rom && m.haddr < (addr_rom + size_rom)); // ROM
    assign s1.hsel = (m.haddr >= addr_ram && m.haddr < (addr_ram + size_ram)); // SRAM
+   assign s2.hsel = (m.haddr[31:30] != 2'b00);                                // I/O
 
    /* HREADY */
    assign m.hready  = hready;
    assign s0.hready = hready;
    assign s1.hready = hready;
+   assign s2.hready = hready;
 
    /* bus connections */
    assign s0.haddr	= m.haddr;
@@ -39,6 +41,15 @@ module hasti_bus
    assign s1.hmastlock	= m.hmastlock;
    assign s1.hwdata	= m.hwdata;
 
+   assign s2.haddr	= m.haddr;
+   assign s2.hwrite	= m.hwrite;
+   assign s2.hsize	= m.hsize;
+   assign s2.hburst	= m.hburst;
+   assign s2.hprot	= m.hprot;
+   assign s2.htrans	= m.htrans;
+   assign s2.hmastlock	= m.hmastlock;
+   assign s2.hwdata	= m.hwdata;
+
    /* multiplexor */
    always_comb
      unique case (1'b1)
@@ -56,6 +67,13 @@ module hasti_bus
 	    hready   = s1.hreadyout;
 	 end
 
+       hsel_r[2]:
+	 begin
+	    m.hrdata = s2.hrdata;
+	    m.hresp  = s2.hresp;
+	    hready   = s2.hreadyout;
+	 end
+
        /* default slave */
        default
 	 begin
@@ -70,5 +88,5 @@ module hasti_bus
      if (!hresetn)
        hsel_r <= '0;
      else
-       hsel_r <= {s1.hsel, s0.hsel};
+       hsel_r <= {s2.hsel, s1.hsel, s0.hsel};
 endmodule
